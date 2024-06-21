01230ecf7a62f7bcf7006ca9b48e0b83bc16ed5c
DeukOsef <doriandeuk@hotmail.fr>
2024-06-21 12:35:47 +0200

    checkpoint 1

---
+++ database.sql
@@ -0,0 +1,14 @@
+insert into bribe (name,payment) 
+VALUES 
+('sam burns', 100),
+('dorian burns',134),
+('simon burns',543),
+('tom burns',5843),
+('julien burns',3434),
+('jean burns',987),
+('benoit burns',4673),
+('monty burns',4738),
+('antoine burns',4672),
+('paul burns',234),
+('quentin burns',4324),
+('remi burns',564);
--- public/book.php
+++ public/book.php
@@ -1,5 +1,7 @@
+<?php require __DIR__ . '/../config.php'; ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -7,28 +9,108 @@
     <link rel="stylesheet" href="css/book.css">
     <title>Checkpoint PHP 1</title>
 </head>
+
 <body>
 
-<?php include 'header.php'; ?>
+    <?php include 'header.php';
+    require_once("../config.php");
 
-<main class="container">
+    $connection = new PDO("mysql:host=" . SERVER . ";dbname=" . DATABASE . ";charset=utf8", USER, PASSWORD);
+    $statement = $connection->query('SELECT name,payment FROM bribe');
+    $bribes = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    <section class="desktop">
-        <img src="image/whisky.png" alt="a whisky glass" class="whisky"/>
-        <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky"/>
 
-        <div class="pages">
-            <div class="page leftpage">
-                Add a bribe
-                <!-- TODO : Form -->
+    $errors = [];
+
+    if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+        $bribe = array_map('trim', $_POST);
+
+        // Validate data
+        if (empty($bribe['name'])) {
+            $errors[] = 'The name is required';
+        }
+        if (empty($bribe['payment'])) {
+            $errors[] = 'The payment is required';
+        }
+        if (!empty($bribe['name']) && $bribe['payment'] < 1) {
+            $errors[] = 'The payment should be more than 0 €';
+        }
+
+        if (empty($errors)) {
+            $query = 'INSERT INTO bribe(name, payment) VALUES (:name, :payment)';
+            $statement = $connection->prepare($query);
+            $statement->bindValue(':name', $bribe['name'], PDO::PARAM_STR);
+            $statement->bindValue(':payment', $bribe['payment'], PDO::PARAM_INT);
+            $statement->execute();
+            header('Location: book.php');
+        }
+    }
+
+
+    ?>
+
+    <main class="container">
+        <div class="range">
+                <?php $chars = range('A', 'Z',1);
+                 foreach ($chars as $char) : ?>
+                    <p><a href="?letter=".$char><?= $char ?></a></p>
+                <?php endforeach; ?>
             </div>
+        <section class="desktop">
+            <img src="image/whisky.png" alt="a whisky glass" class="whisky" />
+            <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky" />
 
-            <div class="page rightpage">
-                <!-- TODO : Display bribes and total paiement -->
+            <div class="pages">
+                <div class="page leftpage">
+                    <?php foreach ($errors as $error) : ?>
+                        <p><?= $error ?></p>
+                    <?php endforeach; ?>
+                    Add a bribe
+                    <form action="" method="post">
+                        <div>
+                            <label for="name">Name</label>
+                            <input type="text" id="name" name="name">
+                        </div>
+                        <div>
+                            <label for="payment">Payment</label>
+                            <input type="text" id="payment" name="payment">
+                        </div>
+                        <div>
+                            <input type="submit" value="Pay !">
+                        </div>
+                    </form>
+                </div>
+
+                <div class="page rightpage">
+                    <table>
+                        <tbody>
+
+                            <?php foreach ($bribes as $bribe) : ?>
+                                <tr>
+                                    <th><?= $bribe['name'] ?></th>
+                                    <td><?= $bribe['payment'] ?>€</td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Total</th>
+                                <td><?php
+                                    $sum = 0;
+                                    foreach ($bribes as $bribe) {
+                                        $sum += $bribe['payment'];
+                                    }
+                                    echo ($sum) ?>€ </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+
+
+                </div>
             </div>
-        </div>
-        <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
-    </section>
-</main>
+            <img src="image/inkpen.png" alt="an ink pen" class="inkpen" />
+        </section>
+    </main>
 </body>
+
 </html>
